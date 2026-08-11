f3253044b053e4f6e8b4eb6ff2ac7d08612dd80d
Cardcarlos <carlos.silva.oliveira09@escola.pr.gov.br>
2026-08-11 09:56:08 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const bottrs = document, querySelectronALL('bottrs')
+
+bottors.forEach(botao => {
+    botao.addEventListener("click",  curtir)
+
+    function curtir() {
+        let curtidas  = document, querySelectronALL("spam")
+        curtidas, textContext+
+    }
+});
