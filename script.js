d222f32a264a2f4ae81f94d5532953d50a356356
Cardcarlos <carlos.silva.oliveira09@escola.pr.gov.br>
2026-08-13 09:54:30 -0300

    botao

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const bottrs = document, querySelectronALL('bottrs')
+const bottors = document.querySelectorAll('button')
 
 bottors.forEach(botao => {
     botao.addEventListener("click",  curtir)
-
+    let curtiu = false
     function curtir() {
-        let curtidas  = document, querySelectronALL("spam")
-        curtidas, textContext+
+        let curtidas  = document.querySelector("span")
+        let curtidas = document.querySelector
+
+       if (curtiu == false){
+           curtiu = true
+           curtidas.textContext++
+           botao.style.backgroundColor == 'green'
+       }
+
     }
 });
